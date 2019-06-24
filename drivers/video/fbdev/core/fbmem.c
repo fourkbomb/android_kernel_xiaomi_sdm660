93f41b96d81a1238ca1a28c6cd6abf5d35363e0a
Simon Shields <simon@lineageos.org>
2019-06-24 23:31:28 +1000

    video: fbdev: avoid calling notifier multiple times

--- drivers/video/fbdev/core/fbmem.c
+++ drivers/video/fbdev/core/fbmem.c
@@ -2,6 +2,7 @@
  *  linux/drivers/video/fbmem.c
  *
  *  Copyright (C) 1994 Martin Schaller
+ *  Copyright (C) 2019 XiaoMi, Inc.
  *
  *	2001 - Documented with DocBook
  *	- Brad Douglas <brad@neruo.com>
@@ -1061,6 +1062,14 @@ fb_blank(struct fb_info *info, int blank)
  	if (blank > FB_BLANK_POWERDOWN)
  		blank = FB_BLANK_POWERDOWN;
 
+#ifdef CONFIG_MACH_XIAOMI
+	if (info->blank == blank) {
+		if (info->fbops->fb_blank) {
+			ret = info->fbops->fb_blank(blank, info);
+		}
+		return ret;
+	}
+#endif
 	event.info = info;
 	event.data = &blank;
 
@@ -1080,6 +1089,10 @@ fb_blank(struct fb_info *info, int blank)
 			fb_notifier_call_chain(FB_R_EARLY_EVENT_BLANK, &event);
 	}
 
+#ifdef CONFIG_MACH_XIAOMI
+	if (!ret)
+		info->blank = blank;
+#endif
  	return ret;
 }
 EXPORT_SYMBOL(fb_blank);
@@ -1641,6 +1654,9 @@ static int do_register_framebuffer(struct fb_info *fb_info)
 		if (!registered_fb[i])
 			break;
 	fb_info->node = i;
+#ifdef CONFIG_MACH_XIAOMI
+	fb_info->blank = -1;
+#endif
 	atomic_set(&fb_info->count, 1);
 	mutex_init(&fb_info->lock);
 	mutex_init(&fb_info->mm_lock);
