fcf16a19133177b07a7c4e45d83a8f661e1c140b
Simon Shields <simon@lineageos.org>
2019-07-11 15:54:26 +1000

    fixup! b0ee41fc76d5

--- drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
+++ drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
@@ -32,7 +32,7 @@ DEFINE_MSM_MUTEX(msm_actuator_mutex);
 #define PARK_LENS_MID_STEP 3
 #else
 #define PARK_LENS_LONG_STEP 7
-#define PARK_LENS_LONG_STEP 5
+#define PARK_LENS_MID_STEP 5
 #endif
 #define PARK_LENS_SMALL_STEP 3
 #define MAX_QVALUE 4096
